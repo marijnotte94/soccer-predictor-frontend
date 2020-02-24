681d901829a3d5d1ba9f810dbfd9ff6d92c4674f
marijnotte94 <marijnotte94@gmail.com>
2020-02-24 15:35:08 +0100

    search matches by date

--- static/matches.js
+++ static/matches.js
@@ -1,10 +1,17 @@
 angular.module('matchModule', ['ngRoute','requestsModule'])
     .controller('matchController', function (httpRequestsService) {
         var vm = this;
-        httpRequestsService.getMatches()
-            .then(function (response) {
-                vm.matches = response.data;
-            });
+        vm.updateData = function(){
+            var date = vm.dateselector;
+            var day = date.getDate();
+            var month = date.getMonth()+1;
+            var year = date.getFullYear();
+            httpRequestsService.getMatches(year, month, day)
+                .then(function (response) {
+                    vm.matches = response.data;
+                });
+
+        };
 
         var now = new Date();
         var loadComp1Time = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 1, 0, 0, 0) - now;
@@ -22,8 +29,26 @@ angular.module('matchModule', ['ngRoute','requestsModule'])
         setTimeout(function(){httpRequestsService.loadComp1()}, loadComp1Time);
         setTimeout(function(){httpRequestsService.loadComp2()}, loadComp2Time);
         setTimeout(function(){httpRequestsService.loadOdds()}, loadOddTime);
+    })
 
+    .filter('byHomeStake', function() {
+        return function(matches){
+            if(matches){
+                return matches.filter(function(match){
+                    return match.homeStake > 0;
+                })
+            }
+        }
+    })
 
-
-
+    .filter('byAwayStake', function() {
+        return function(matches){
+            if(matches){
+                return matches.filter(function(match){
+                    return match.awayStake > 0;
+                })
+            }
+        }
     });
+
+
--- static/requests.js
+++ static/requests.js
@@ -1,10 +1,10 @@
 angular.module('requestsModule', [])
     .factory('httpRequestsService', function($http) {
         return {
-            getMatches: function () {
+            getMatches: function (year, month, day) {
                 return $http({
                     method: "GET",
-                    url: "https://mathbettingtips.herokuapp.com/matches/today"
+                    url:  "https://mathbettingtips.herokuapp.com/matches?year="+year+"&month="+month+"&day="+day
                 })
             },
             loadComp1: function() {
